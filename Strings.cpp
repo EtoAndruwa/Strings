47a7da418bb16b38fae57c701fb21302282e5e9a
EtoAndruwa <timonina1909@gmail.com>
2022-11-24 01:23:44 +0300

    Добавил strncat, начал писать комментарии для функций

--- Strings.cpp
+++ Strings.cpp
@@ -1,30 +1,27 @@
 #include <stdio.h>
 
-void puts_my(const char* str);
-int strlen_null_my(const char* str);
-int strlen_no_null_my(const char* str);
+void puts_my(const char* str); // prints string with \n in the end of string
+size_t strlen_null_my(const char* str);
+size_t strlen_no_null_my(const char* str);
 const char* strchr_my(const char* str, char chr);
-char* strcpy_my(char* dest, int dest_length, const char* src);
-void strncpy_my(char* dest, int dest_length, int count, const char* src);
-char* strcat_my(char* dest, const char *src, int max_length);
+char* strcpy_my(char* dest, size_t dest_length, const char* src);
+void strncpy_my(char* dest, size_t dest_length, const char* src);
+char* strcat_my(char* dest, const char* src, size_t max_length);
+char* strncat_my(char* dest, const char* src, size_t count);
 
 int main()
 {
     char string[15] = "Hello World";
     char copy[18] = "Hi";
 
-    printf("%s\n", strcat_my(copy, string, sizeof(copy)));
-    printf("%p\n", strcat_my(copy, string, sizeof(copy)));
-
-
     return 0;
 }
 
-void puts_my(const char* str)// DONE
+void puts_my(const char* str)// prints string with \n in the end of string
 {
     size_t lenght = strlen_null_my(str);
 
-    if (lenght != 0)
+    if (lenght != 0)// if success prints string
     {
         for (size_t i = 0; i < lenght; i++)
         {
@@ -32,9 +29,13 @@ void puts_my(const char* str)// DONE
         }
         putchar('\n');
     }
+    else // if failure prints EOF
+    {                               
+        printf("%d", EOF);          
+    }                              
 }
 
-int strlen_null_my(const char* str)// with \0 DONE
+size_t strlen_null_my(const char* str)// with \0 DONE
 {
     size_t i = 0;
     while (str[i] != '\0')
@@ -45,7 +46,7 @@ int strlen_null_my(const char* str)// with \0 DONE
     return i;
 }
 
-int strlen_no_null_my(const char* str) // without \0 DONE
+size_t strlen_no_null_my(const char* str) // without \0 DONE
 {
     size_t i = 0;
     while (str[i] != '\0')
@@ -119,8 +120,6 @@ void strncpy_my(char* dest, size_t dest_length, const char* src)
 char* strcat_my(char* dest, const char* src, size_t max_length)//DONE
 {
     size_t position_to_write = strlen_null_my(dest) - 1;
-    //printf("Max length of text = %d\n", max_length);
-    //printf("Position to write = %d\n", position_to_write);
 
     if(max_length >= (strlen_null_my(src) + strlen_null_my(dest)))
     {   
@@ -137,8 +136,17 @@ char* strcat_my(char* dest, const char* src, size_t max_length)//DONE
     }
 }
 
-char* strncat_my( char* dest, const char* src, size_t count)
+char* strncat_my( char* dest, const char* src, size_t count)//DONE
 {
     size_t position_to_write = strlen_null_my(dest) - 1;
+    for(size_t i = 0; i < count; i++)
+    {
+        dest[position_to_write] = src[i];
+        position_to_write++;
+    }
+    position_to_write++;
+    dest[position_to_write] = '\0';
+
+    return dest;
 }
 

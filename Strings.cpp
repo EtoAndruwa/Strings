b83f0bd0ab02a4c1154cf5a21aaaa7467e55a984
EtoAndruwa <timonina1909@gmail.com>
2022-11-24 03:01:11 +0300

    Добавил комментарии ко всем существующим функциям, добавил отладочную печать, проверил все функции

--- Strings.cpp
+++ Strings.cpp
@@ -9,11 +9,14 @@ size_t strlen_no_null_my(const char* str); // returns the length of the string, 
 const char* strchr_my(const char* str, char chr); // returns the pointer to the found character in the given string
                                                   // or returns the nullptr if the character was not found
 
-char* strcpy_my(char* dest, size_t dest_length, const char* src);
+char* strcpy_my(char* dest, size_t size_of_dest, const char* src); // (modified by me) returns the pointer to the string dest into which
+                                                                  // the string src was copied, checks the string dest for enough space
 
-void strncpy_my(char* dest, size_t dest_length, const char* src);
+char* strncpy_my(char* dest, const char* src, size_t count); // returns the pointer to the string dest into which
+                                                             // the given number of characters from the string src were copied
+                                                                                 
 
-char* strcat_my(char* dest, const char* src, size_t max_length); // (modified by me) returns the pointer to the string dest, checks 
+char* strcat_my(char* dest, const char* src, size_t size_of_dest); // (modified by me) returns the pointer to the string dest, checks 
                                                                  // for enough space and appends the copy of the string src 
 
 char* strncat_my(char* dest, const char* src, size_t count); // returns the pointer to the string dest, copies the entered 
@@ -23,17 +26,34 @@ int main()
 {
     char string[15] = "Hello World";
     char copy[18] = "Hi";
+    char text1[10] = "Pri";
+    char text2[10] = "vet";
+    char text3[10] = "Mir";
+    char text4[15] = " Mir! Yra!";
+    char text5[12] = "Home";
+    char text6[15] = "Works";
+    
+    printf("Result of puts(copy): ");
+    puts_my(copy);
+    printf("Length without 0: %ld\n", strlen_no_null_my(copy));
+    printf("Length with 0: %ld\n", strlen_null_my(copy));
+    printf("Character found: %c\n", *strchr_my(copy, 'H'));
+    printf("New string 'copy': %s\n", strcpy_my(copy, sizeof(copy), string));
+    printf("Result of strcat(text1, text2): %s\n", strcat_my(text1, text2, sizeof(text1)));
+    printf("Result of strncat(text3, text4): %s\n", strncat_my(text3, text4, 5));
+    printf("Result of strncopy(text5, text6): %s\n", strncpy_my(text5, text6, 6));
+
 
     return 0;
 }
 
 void puts_my(const char* str) // prints the string with the charecter '\n' in the end of the string.
 {
-    size_t lenght = strlen_null_my(str);
+    size_t length = strlen_null_my(str);
 
-    if (lenght != 0)
+    if (length != 0)
     {
-        for (size_t i = 0; i < lenght; i++)
+        for (size_t i = 0; i < length; i++)
         {
             putchar(str[i]);
         }
@@ -92,10 +112,10 @@ const char* strchr_my(const char* str, char chr) // returns the pointer to the f
     }
 }
 
-char* strcpy_my(char* dest, size_t dest_length, const char* src) // DONE
-{
+char* strcpy_my(char* dest, size_t size_of_dest, const char* src) // (modified by me) returns the pointer to the string dest into which 
+{                                                                // the string src was copied, checks the string dest for enough space
     size_t leght_scr = strlen_null_my(src);
-    if (dest_length >= leght_scr)
+    if (size_of_dest >= leght_scr)
     {
         for (size_t i = 0; i <= leght_scr; i++)
         {
@@ -109,26 +129,42 @@ char* strcpy_my(char* dest, size_t dest_length, const char* src) // DONE
     }
 }
 
-void strncpy_my(char* dest, size_t dest_length, const char* src)
-{
-    if (dest_length >= strlen_null_my(src))
+char* strncpy_my(char* dest, const char* src, size_t count) // returns the pointer to the string dest into which the
+{                                                           // given number of characters from the string src were copied
+    if (strlen_null_my(src) < count)                        
     {
-        for (size_t i = 0; i < strlen_null_my(src); i++)
-        {
+        for (size_t i = 0; i < count; i++)
+        {   
+            if(i < strlen_null_my(src))
+            {
+                dest[i] = src[i];
+            }
+            else
+            {
+                dest[i] = '\0';   
+            }
+        }
+        return dest;
+    }
+    else if(strlen_null_my(src) >= count)
+    {
+        for (size_t i = 0; i < count; i++)
+        {   
             dest[i] = src[i];
         }
+        return dest;
     }
     else
     {
-        
+        return dest;
     }
 }
 
-char* strcat_my(char* dest, const char* src, size_t max_length) // returns the pointer to the string dest, appends the copy of 
+char* strcat_my(char* dest, const char* src, size_t size_of_dest) // (modified by me) returns the pointer to the string dest, checks
 {                                                               // for enough space and appends the copy of the string src
     size_t position_to_write = strlen_null_my(dest) - 1;
 
-    if(max_length >= (strlen_null_my(src) + strlen_null_my(dest))) // (modified by me) returns the pointer to the string dest, checks 
+    if(size_of_dest >= (strlen_null_my(src) + strlen_null_my(dest))) 
     {   
         for(size_t i = 0; i < strlen_null_my(src); i++)
         {
